b60907026e07afa71256053faa31c0f6ef389e4b
Arsh Radhanpura <arshradhanpura28@gmail.com>
2020-09-24 10:33:46 +0530

    change text view only to edit text

--- app/src/main/java/com/example/convertor/resultActivity.java
+++ app/src/main/java/com/example/convertor/resultActivity.java
@@ -7,21 +7,24 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.text.BreakIterator;
+
 public class resultActivity extends AppCompatActivity
 {
-    TextView headerText,resultText;
+    TextInputEditText result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
-        Intent intent=getIntent();
+        Intent intent = getIntent();
         Bundle bundle = getIntent().getExtras();
         String ans = bundle.getString("textResult");
         //String ans = intent.getStringExtra("textObtained");
         Log.i("textob=",""+ans);
-        resultText=findViewById(R.id.resultText);
-        resultText.setText(ans);
-
+        result=findViewById(R.id.result);
+        result.setText(ans);
     }
 }
